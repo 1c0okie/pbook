15581615e3fae4215be739f5397ea235756b8ce6
1c0okie <tbthanh00@gmail.com>
2026-05-14 03:42:01 +0700

    return raw

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -2,18 +2,11 @@ import nodemailer from 'nodemailer';
 
 export const sendEmail = async ({ to, subject, html }) => {
   const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587, // Đã đổi sang cổng 587
-    secure: false, // Cổng 587 bắt buộc secure phải là false
+    service: 'gmail',
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
-    family: 4, // Vẫn giữ nguyên để chống lỗi ENETUNREACH (IPv6)
-    connectionTimeout: 10000, 
-    tls: {
-      rejectUnauthorized: false // Bỏ qua lỗi xác thực chứng chỉ môi trường cloud
-    }
   });
 
   const mailOptions = {
