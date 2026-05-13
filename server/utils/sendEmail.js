ab7f6d9649312fd9a42b033ca70cca76ce6d6231
1c0okie <tbthanh00@gmail.com>
2026-05-14 02:27:59 +0700

    Configure sendemail

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -2,11 +2,19 @@ import nodemailer from 'nodemailer';
 
 export const sendEmail = async ({ to, subject, html }) => {
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    // THAY THẾ dòng service: 'gmail' bằng các dòng dưới đây
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, // dùng cổng 465 thì secure là true
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
+    // THÊM thuộc tính này để giải quyết lỗi ENETUNREACH trên Render
+    family: 4, 
+    tls: {
+      rejectUnauthorized: false
+    }
   });
 
   const mailOptions = {
