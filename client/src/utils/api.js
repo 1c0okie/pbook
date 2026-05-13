e1e9229875f76b1e287404270151208eb5ef5c98
1c0okie <tbthanh00@gmail.com>
2026-05-13 23:55:12 +0700

    fix api

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'import.meta.env.VITE_API_URL', // URL Backend
+  // Đã bỏ dấu nháy đơn '' ở dòng dưới đây
+  baseURL: import.meta.env.VITE_API_URL, 
   headers: {
     'Content-Type': 'application/json',
   },
